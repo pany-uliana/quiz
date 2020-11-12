0f036f3d7e7a6c18e84a56fe3445a66c231608d2
pany-uliana <uliana.solovova@yandex.ru>
2020-11-12 17:02:50 +0300

    pre-release

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ import random
 import csv
 import sqlite3
 import time
-# import chardet
+import chardet
 
 
 # with open('quiz.csv', 'rb') as file:
@@ -27,6 +27,18 @@ def get_data_from_file():
         for i in file:
             questions.append(i)
     return questions
+
+
+def get_unique_data(topic_id, answer):
+    con = sqlite3.connect('quiz_db.db')
+    cur = con.cursor()
+    query = "SELECT * FROM qa WHERE topic_id = (?) and question NOT LIKE (?) GROUP BY (question)"
+    args = (topic_id, answer)
+    res = cur.execute(query, args).fetchall()
+    con.commit()
+    con.close()
+
+    return res
 
 
 def get_result():
@@ -75,9 +87,13 @@ class Start(QWidget):
         self.setWindowTitle('Тест')
 
     def setup(self):
+        self.greeting = QLabel(self)
+        self.greeting.setGeometry(QRect(70, -50, 250, 250))
+        self.greeting.setText('Выберите нужный тест.')
+        self.greeting.setFont(QtGui.QFont('Arial', 12))
         self.cb = QComboBox(self)
-        self.cb.move(50, 60)
-        self.cb.setPlaceholderText('выберите тест:')
+        self.cb.move(70, 100)
+        self.cb.setPlaceholderText('Выбрать тест:')
         for i in get_topics():
             self.cb.addItem(i[1], i[0])
         self.cb.currentIndexChanged.connect(self.selectionchange)
@@ -97,8 +113,8 @@ class Example(QWidget):
         self.step = 1
         self.user_answers = []
         self.topic_id = topic_id
-
-        self.length = len(get_data(topic_id))
+        self.counter = 0
+        self.length = len(get_data(self.topic_id))
 
         self.test(self.step)
 
@@ -106,10 +122,11 @@ class Example(QWidget):
         self.setGeometry(100, 100, 650, 300)
         self.setWindowTitle('Тест')
         self.input_first = QLabel(self)
-        self.input_first.setText('Вопрос:')
-        self.input_first.move(50, 40)
+        self.input_first.setGeometry(QRect(50, -10, 250, 100))
+        self.input_first.setText('Вопрос')
+        self.input_first.setFont(QtGui.QFont('Arial', 13))
         self.question = QLabel(self)
-        self.question.move(50, 60)
+        self.question.move(50, 70)
 
         self.layout = QFrame(self)
         self.init_button()
@@ -121,68 +138,74 @@ class Example(QWidget):
         data = get_data(self.topic_id)
 
         self.question.setText(data[step][2])
-        self.question.setGeometry(QRect(50, 60, 600, 70))
+        self.question.setGeometry(QRect(50, 52, 600, 80))
         font = QtGui.QFont()
-        font.setPointSize(12)
+        font.setPointSize(13)
         self.question.setFont(font)
         self.question.setWordWrap(True)
 
         numbers = (self.length - 1 if self.length < 4 else 3)
         correct = data[step]
-        del data[step]
-        failed = random.sample(data, numbers)
+
+        failed = random.sample(get_unique_data(self.topic_id, correct[1]), numbers)
         for i in random.sample(failed + [correct], numbers + 1):
             a += 30
             j += 1
             button_name = "radioButton" + str(j)
             button_name = QRadioButton(self.layout)
-            if correct[0] != i[0]:
-                self.button_group.addButton(button_name, correct[0])
+            self.button_group.addButton(button_name, correct[0])
             button_name.setGeometry(QRect(50, 100 + a, 255, 17))
             button_name.setObjectName("radioButton" + str(j))
             button_name.setText(i[1])
             button_name.toggled.connect(self.get_answer)
 
             button_name.show()
+        self.counter += 1
+        self.input_first.setText('Вопрос № ' + str(self.counter) + ' из ' + str(self.length - 1))
 
     def get_answer(self):
         self.btn.setEnabled(True)
 
     def init_button(self):
-        self.btn = QPushButton('->', self)
+        self.btn = QPushButton('Перейти к следующему вопросу.', self)
         self.btn.setEnabled(False)
-        self.btn.resize(40, 40)
-        self.btn.move(60, 250)
+        self.btn.resize(200, 40)
+        self.btn.move(50, 250)
         self.btn.clicked.connect(self.validate)
 
     def validate(self):
         for children in self.layout.children():
             if children.isChecked() and self.button_group.id(children) > 0:
                 self.user_answers.append(self.button_group.id(children))
-                self.layout.setStyleSheet("border: 1px solid green;")
-            else:
-                self.layout.setStyleSheet("border: 1px solid red;")
             children.hide()
-            # self.button_group.removeButton(children)
+            self.button_group.removeButton(children)
 
         self.btn.setEnabled(False)
         self.step += 1
         if self.step < self.length:
             self.test(self.step)
         else:
-            # print(self.user_answers)
-            self.conclusion()
+            if self.user_answers:
+                self.input_first.setText('Ошибки:')
+                self.conclusion()
+            else:
+                self.input_first.setText('Ошибок нет.')
+                self.conclusion()
 
     def conclusion(self):
         con = sqlite3.connect('quiz_db.db')
         cur = con.cursor()
-        query = cur.execute("SELECT * FROM qa WHERE id in (%s)" % (', '.join(str(id) for id in self.user_answers))).fetchall()
+        query = cur.execute("SELECT * FROM qa WHERE id in (%s)" %
+                            (', '.join(str(i) for i in self.user_answers))).fetchall()
         con.commit()
         con.close()
         text = ''
         for i in query:
             text += i[1] + ' — ' + i[2] + '\n'
         self.question.setText(text)
+        self.question.move(50, 75)
+        self.btn.setText('Вернуться на главную страницу.')
+        self.btn.setEnabled(True)
 
 
 if __name__ == '__main__':
